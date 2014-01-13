815bb2a98caff5a9cc7d57b53b96c6a2460ec748
zaiac <zaiac06@gmail.com>
2014-01-13 21:23:07 +0700

    Add models, misc_page, css, update settings (all files)

--- blogOfMaps/blogOfMaps/models.py
+++ blogOfMaps/blogOfMaps/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Users(User):
+    user_pic = models.ForeignKey('MediaObject')
+
+
+class Story(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    title = models.CharField(max_length=255, default=None, blank=True, null=True)
+    text = models.TextField()
+    author = models.ForeignKey(Users, related_name='storyes')
+    country = models.ForeignKey('Country', related_name='storyes')
+    public = models.BooleanField(default=False)
+    version_last = models.ForeignKey('StoryVersion', related_name='story')
+
+    def __unicode__(self):
+        return self.title
+
+
+class StoryVersion(Story):
+    edited_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255, default=None, blank=True, null=True)
+    eng_name = models.CharField(max_length=255)
+    story_count = models.IntegerField()
+    point = models.ForeignKey('Point')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Point(models.Model):
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    comment = models.TextField()
+    author = models.ForeignKey(Users)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+
+
+class MediaObject(models.Model):
+    file_name = models.CharField(max_length=255)
+    file_ext = models.CharField(max_length=4)
+    file_hash = models.CharField(max_length=255, editable=False, blank=True, null=True)
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    deleted = models.BooleanField(default=False)
+    story = models.ForeignKey(Story, related_name='media')
+    author = models.ForeignKey(Users, related_name='media')
+
+    def __unicode__(self):
+        return self.file_name
+
+
+class Comments(models.Model):
+    story = models.ForeignKey(Story, related_name='comment')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, blank=True)
+    author = models.ForeignKey(Users, related_name='comment')
--- blogOfMaps/blogOfMaps/settings.py
+++ blogOfMaps/blogOfMaps/settings.py
@@ -1,4 +1,5 @@
 # Django settings for blogOfMaps project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,12 +12,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog_maps',
+        'USER': 'blog_maps',
+        'PASSWORD': 'pypassw0rd',
+        'HOST': 'localhost',
         'PORT': '',                      # Set to empty string for default.
     }
 }
@@ -48,14 +48,16 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+ROOT = os.path.dirname(__file__)
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(ROOT, os.path.pardir, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -111,6 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(ROOT, os.path.pardir, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -118,12 +121,14 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.messages',
+    #'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'blogOfMaps',
+    'south'
 )
 
 # A sample logging configuration. The only tangible logging
